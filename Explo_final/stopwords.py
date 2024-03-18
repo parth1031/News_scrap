47685beb5abacd35bcdabf2dff2bd5069b57cf54
Zsclarx <117422450+Zsclarx@users.noreply.github.com>
2024-03-18 11:53:18 +0530

    Update stopwords.py

--- Explo_final/stopwords.py
+++ Explo_final/stopwords.py
@@ -3,7 +3,7 @@ import re
 
 
 def stop_word_lis():
-    f= open(r"C:\Users\Prakhar\OneDrive\Desktop\stop_words_en.txt")
+    f= open(r"stop_words_en.txt")
     fh = f.read()
     stop_words = []
     for word in fh:
